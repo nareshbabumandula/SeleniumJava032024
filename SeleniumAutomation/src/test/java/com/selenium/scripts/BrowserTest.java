9675c4e2cc22d6eddc9ee9f8c85c433bc8398364
nareshbabumandula <naresh223@gmail.com>
2024-03-13 08:56:59 +0530

    13-Mar-2024: Naresh: Important Interfaces and Classes in Selenium Architecture and explanation for Selenium WebDriver Code

--- SeleniumAutomation/src/test/java/com/selenium/scripts/BrowserTest.java
+++ SeleniumAutomation/src/test/java/com/selenium/scripts/BrowserTest.java
@@ -1,7 +1,13 @@
 package com.selenium.scripts;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class BrowserTest {
 
@@ -10,8 +16,28 @@ public class BrowserTest {
 		driver = new ChromeDriver();
 		driver.get("https://www.mycontactform.com");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
+		driver.findElement(By.id("user")).sendKeys("Saikrishna");
+		driver.findElement(By.id("pass")).sendKeys("Secure*123");
+	
+		WebElement username = driver.findElement(By.id("user"));
+		System.out.println(username.isDisplayed());
+		System.out.println(username.isEnabled());
+		System.out.println(username.isSelected());
+		
+		username.clear();
+		username.sendKeys("Sumithra");
+		
+		List<WebElement> elements = driver.findElements(By.xpath("//input[@class='txt_log']"));
+		System.out.println("No of elements found are : " + elements.size());
+		
+		for (WebElement textfield : elements) {
+			System.out.println(textfield.getAttribute("name"));
+			System.out.println(textfield.getAttribute("type"));
+		}
+			
 		Thread.sleep(3000);
 		driver.quit();
 	}
